7b826effa934ab596a9fb1aa7c63636d286e8b65
Kinjal Mudgal <mudgalkinjal@gmail.com>
2025-01-27 23:05:38 -0500

    added allowedorigins

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,27 +3,53 @@ import { createServer } from 'http'
 import { Server } from 'socket.io'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import http from 'http'
 import connectDB from './config/db'
 import authRoutes from './routes/auth'
 import chatRoutes from './routes/chat'
 import transporter from './config/transporter'
 
-// Load environment variables
+transporter.verify((error, success) => {
+  if (error) {
+    console.error('SMTP connection error:', error)
+  } else {
+    console.log('SMTP connection successful:', success)
+  }
+})
+
 dotenv.config()
-//process.env.CLIENT_URL
 
 const app = express()
 const server = createServer(app)
 
+const allowedOrigins = ['http://localhost:3000', process.env.CLIENT_URL]
+
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
     methods: ['GET', 'POST'],
+    credentials: true,
   },
 })
 
-app.use(cors())
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    credentials: true,
+  })
+)
+
 app.use(express.json())
 connectDB()
 
@@ -39,19 +65,8 @@ io.on('connection', (socket) => {
   })
 })
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.error('SMTP connection error:', error)
-  } else {
-    console.log('SMTP connection successful:', success)
-  }
-})
-
-// Mount routes
 app.use('/api/auth', authRoutes)
 app.use('/api/chat', chatRoutes)
-
-// Health Check Route
 app.get('/', (req, res) => {
   res.send('Server is running')
 })
